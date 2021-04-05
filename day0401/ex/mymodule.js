0650bf4941af4b428fb15b9946d61cf7da92a954
leejihyeon1 <55727503+leejihyeon1@users.noreply.github.com>
2021-04-05 17:57:01 +0900

    Add files via upload

---
+++ day0401/ex/mymodule.js
@@ -0,0 +1,20 @@
+/*
+모듈은 변수, 함수 등의 코드를 모아놓고 파일로 저장한 단위(lib같은 것) 
+개발자가 모듈을 정의할때는 내장객체 중 exports 객체를 사용하면 됨
+*/
+
+//getMsg 메서드를 현재 모듈 안에 정의한다!
+exports.getMsg=function(){ //export는 모듈을 만들때 사용
+    return "this message is from mymodule";
+}
+
+//랜덤값 가져오기
+exports.getRandom=function(n){
+    var r=parseInt(Math.random()*n);
+    return n;
+}
+
+exports.getZeroString=function(n){
+    var result=(n>=10)? n:"0"+n;
+    return result;
+}
